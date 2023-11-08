3b5f4409afb6ecdd8be31273be43cb34dc30c022
Jan Michael Biyo <janmichael.biyo@gmail.com>
2023-11-08 04:11:14 -0600

    doing some loops

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -14,16 +14,25 @@ for (let i=0; i<4; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 
+for (let x=0; x<6; x++){console.log(x)};
+
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+
+for (let x=3; x<6; x++){console.log(x)};
+
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
+for (let x=2; x<12; x+=2){console.log(x)};
+
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
+
+for (let x=5; x>=0; x--){console.log(x)};
 
 
 // 2. For of loops  
@@ -33,6 +42,7 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
 
+for (let element of stars){console.log(element)};
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
@@ -40,8 +50,22 @@ console.log('---- 3. While loop ----');
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
 
+let i = 0
+let text = ""
+while (stars[i]) {text += stars[i]; ++i; console.log(text)}
+// i dont think this is write but this is the best i can do for right now
+
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 
+let x = -1
+while (x < 5) { x; ++x; x<5; console.log(x)};
+
+
+
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+
+let y = 11;
+
+while (y>5) {y; --y; console.log(y)}
